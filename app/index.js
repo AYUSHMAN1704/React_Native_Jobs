ca4c9bfc697b677e503e63e66e1c8bbb9ee0a2fa
AYUSHMAN1704 <ayushshishodia63@gmail.com>
2026-07-23 02:57:25 +0530

    Latest Commit

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import { View, SafeAreaView, ScrollView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";

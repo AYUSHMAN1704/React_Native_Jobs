9f8935598867ead3b3d74d656d957490790c2f00
AYUSHMAN1704 <ayushshishodia63@gmail.com>
2026-07-23 14:30:15 +0530

    Hello

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,7 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
-  );
+const Layout = () => {
+    return <Stack />;
 }
+
+export default Layout;
--- app/index.js
+++ app/index.js
@@ -1,9 +1,41 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View , ScrollView} from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
-export default function HomeScreen() {
+const Home = () => {
+    const router = useRouter();
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hello from app/index.js!</Text>
-        </View>
-    );
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension={"60%"} />
+                    },
+                    headerRight: () => {
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension={"100%"} />
+                    },
+                    headerTitle: "",
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex:1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+export default Home;

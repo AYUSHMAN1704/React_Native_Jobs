121ac39420effcc65e7036c94cf3525d0d3018d7
AYUSHMAN1704 <ayushshishodia63@gmail.com>
2026-07-23 10:19:28 +0530

    latest Commit

--- app/index.js
+++ app/index.js
@@ -3,3 +3,16 @@ import { View, SafeAreaView, ScrollView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
 import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home;

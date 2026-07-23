088a46532a34f50f54b4b4d21ba895ca71a40d72
AYUSHMAN1704 <ayushshishodia63@gmail.com>
2026-07-23 10:27:56 +0530

    Clean slate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
-import { View, Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
